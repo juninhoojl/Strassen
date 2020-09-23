268ac9b150f982e823311f471445a7675b86efac
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-09-23 15:25:24 -0300

    Initial Commit

---
+++ Strassen/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Strassen
+//
+//  Created by José Luiz Junior on 23/09/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
